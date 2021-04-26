e2bf0ce9b3ecd326bda287c17af24321cc03cf77
Arco Dielhof <arcodielhof@hotmail.com>
2021-04-26 23:04:54 +0200

    Icons for WoW AddOns.

---
+++ StreamGoal.lua
@@ -0,0 +1,45 @@
+local StreamToDoList =
+{
+    ["EV"] =
+    {
+        --"Wider Vendor Window",
+        "AutoSell Old Content BoPs",
+    },
+    ["IC"] =
+    {
+        "Different Companions List",
+        "Multiple Active Companions",
+    },
+    ["IH"] =
+    {
+        "DeathMarker in List",
+    },
+    ["YT"] =
+    {
+        "Install other Software",
+        "Remornia Model",
+    },
+    ["CR"] =
+    {
+        "VersionControl",
+    },
+}
+print(StreamToDoList)
+
+
+
+Stand [27]
+Stand [28]              Looks around
+Walk [35]               Standard Walk
+ShaRewadySpellCast [54] Better stand animation
+Attack2H [1]            Needs to hold Remornia.
+CombatChargeEnd [52]
+CombatChargeLoop [53]
+CustomSpell02 [64]      Arrogant Pose + talking
+CustomSpell06 [67]      Talking something
+EmoteTalkQuestion [17]  Talking Something
+
+
+
+
+CombatAbility2H01 [4]   Practically a golf animation, WTF?
